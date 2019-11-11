3a3d0d1b576cad496acb6e91f02601d4cacfe920
kajikasama <aguschandravii4@gmail.com>
2019-11-11 22:38:06 +0800

    menambahkan file

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Presentasi Web</title>
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		
+		<section class="jumbotron">
+			<div class="container-fluid text-center">
+				<img src="img/logo.png" class="text-center">
+				<h1>I Putu Agus Chandra Dani Harta</h1>
+				<h1>XI RPL 4</h1>
+			</div>
+		</section>
+		<section class="latarbelakang text-center">
+			<div class="container-fluid">
+				<h2>Latar Belakang</h2>
+				<hr>
+				<img class="img-responsive" src="img/alurkerja.jpg" alt="">
+				<br>
+				<br>
+				<br>
+				<hr class="panjang">
+			</div>
+		</section>
+		<section class="perusahaan text-center">
+			<div class="container-fluid">
+				<h2>Technologia Creativa</h2>
+				<hr>
+				<img src="img/logoperusahaan.png" alt="">
+			</div>
+			<div class="container">
+				<ul type="1">
+					<li>Bali Happy Events</li>
+					<img src="img/produk-produk/balihappyevents.PNG" alt="">
+					<li>Pt Touchwood</li>
+					<img src="img/produk-produk/PtTouchwood.PNG" alt="">
+					<li>Villa Lotus Bali</li>
+					<img src="img/produk-produk/VillaLotusBali.PNG" alt="">
+				</ul>
+			</div>
+		</section>
+		<section class="pelaksanaan">
+			<div class="container text-center">
+				<h2 class="text-center">Pelaksanaan Prakrin</h2>
+				<hr>
+				<h3>Ringkasan Pelaksanaan Kegiatan Prakrin Antara Lain : </h3>
+				<ul style="padding-left: 340px;" type="&bull;" class="text-left">
+					<li>Pelepasan dan Pengarahan</li>
+					<li>Mendesain Website</li>
+					<li>Menambahkan fitur dan atribut pada website </li>
+					<li>Membuat website dengan fitur system login</li>
+					<li>Menggunakan Framework Bootstrap untuk membuat website responsive</li>
+					<li>Menambahkan input data pada website</li>
+					<li>Memahami domain dan hosting website</li>
+				</ul>
+				<hr class="panjang2">
+			</div>
+		</section>
+		<section class="projectweb text-center">
+			<div class="container">
+				<h2>Project Web</h2>
+				<hr>
+				<h3>Pindah Ke Halaman Web Project :<h3>
+				<a class="btn btn-lg btn-info" href="http://localhost/projectrestaurant2/">The Night Market Restaurant</a>
+			</div>
+		</section>
+		<section class="kesimpulan text-center">
+			<div class="container">
+				<h2>Kesimpulan</h2>
+				<hr>
+				<h4>
+				Dengan adanya pelaksanaan kerja praktek ini, si penulis  memperoleh banyak manfaat yang mungkin tidak diperoleh dalam pelajaran di sekolah. Disini dalam dunia kerja yang sesungguhnya dituntut untuk mandiri sabar dan bertanggung jawab dalam menyelesaikan tugas. Dalam kerja praktek ini berkesempatan menerapkan ilmu yang didapatkan dalam dunia pelajaran di sekolah.
+				</h4>
+			</div>
+		</section>
+		
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<script src="script.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
